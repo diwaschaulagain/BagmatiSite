4de4d92fe297cf27daa73ce0e863ffd8898481b4
diwaschaulagain <chaulagaindiwas@yahoo.com>
2020-10-08 14:44:45 +0545

    Initial Commit

---
+++ BagmatiSite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bagmati.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+        
+
+            return View();
+        }
+        public ActionResult OurBrands()
+        {
+            return View();
+        }
+        public ActionResult Certificates()
+        {
+            return View();
+        }
+        public ActionResult OurCategories()
+        {
+            return View();
+        }
+        public ActionResult OurProducts()
+        {
+            return View();
+        }
+    }
+}
